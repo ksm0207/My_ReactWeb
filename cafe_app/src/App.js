3931c518d1d3456f255678a49dab5ff3df41fdf3
ksm0207 <ksm03071@naver.com>
2020-11-18 20:25:53 +0900

    React 첫번째 구성요소
    - JSX Part1 학습완료
    - 컴포넌트 활용하기

--- cafe_app/src/App.js
+++ cafe_app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
+import Coffee from "./coffee"
 
 function App() {
   return ( 
     <div>
       <h1>안녕하세요 ? </h1>
       <h1>반갑습니다 !!</h1>
+      <Coffee></Coffee>
     </div>
+    
   );
 }
 
---
+++ cafe_app/src/coffee.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Coffee(){
+    return(
+        <div>
+            <h3>이것은 coffee.js 컴퍼넌트 입니다</h3>
+            <h3>JSX</h3>
+        </div>
+    );
+}
+
+export default Coffee;
