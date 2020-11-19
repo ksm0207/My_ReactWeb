a36e5d18dc5c0018c71ed1995cb047ce9b4885e2
ksm0207 <ksm03071@naver.com>
2020-11-20 02:22:05 +0900

    JSX 학습진행 완료
    - 필요없는 파일 삭제
    - JSX Part2 학습완료

--- cafe_app/src/App.js
+++ cafe_app/src/App.js
@@ -1,12 +1,22 @@
 import React from "react"
-import Coffee from "./coffee"
+
+
+function Coffee({coffee_menu}){
+    
+  return (
+    <h1> 나는  {coffee_menu} 를 (을) 주문했어요 </h1>
+  )
+
+}
 
 function App() {
   return ( 
     <div>
       <h1>안녕하세요 ? </h1>
       <h1>반갑습니다 !!</h1>
-      <Coffee></Coffee>
+      <Coffee coffee_menu ="아메리카노"></Coffee>
+      <Coffee coffee_menu ="콜드브루 진한맛"></Coffee>
+      <Coffee coffee_menu ="에스프레소"></Coffee>
     </div>
     
   );
