be334d3dc78cca11b2989a070fee4f064deb7e68
ksm0207 <ksm03071@naver.com>
2020-12-01 03:40:04 +0900

    Function 이름 수정

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component{
   }
   // https://yts-proxy.nomadcoders1.now.sh/list_movies.json
 
-  getMoveis = async() =>{
+  getMovies = async() =>{
     // 영화 데이터를 가진 API Function
     const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
     console.log(movies)
@@ -16,7 +16,7 @@ class App extends React.Component{
 
   componentDidMount(){
     // 컴포넌트가 생성된후 getMovies() 함수를 호출합니다
-    this.getMoveis();
+    this.getMovies();
   }
   
   render(){
