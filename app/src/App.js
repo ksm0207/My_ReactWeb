dd48307990f7b1c0581c55cbcba30ae14ce718a6
ksm0207 <ksm03071@naver.com>
2020-12-02 14:12:12 +0900

    Movie title 넣기완료
    - API 데이터 학습중

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,22 @@
 import React from "react"
 import axios from "axios"
+import Movie from "./Movie"
 
 class App extends React.Component{
   state = {
     isLoading: true,
-    items : []
+    movies : []
   }
-  // https://yts-proxy.nomadcoders1.now.sh/list_movies.json
 
   getMovies = async() =>{
     // 영화 데이터를 가진 API Function
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
-    console.log(movies)
-  }
-
+    const {
+      data:{
+        data:{movies}
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+    this.setState({movies, isLoading : false})
+  };
   componentDidMount(){
     // 컴포넌트가 생성된후 getMovies() 함수를 호출합니다
     this.getMovies();
@@ -21,11 +24,19 @@ class App extends React.Component{
   
   render(){
     const isLoading = this.state.isLoading
+    const movies = this.state.movies
     return(
       <div>
-        {isLoading ? "불러오는중 ... " : "준비완료"}
+        {
+          isLoading ? "불러오는중 ..." :
+          movies.map(movie =>(
+            <Movie
+              title = {movie.title}     
+            />
+          ))}
+        
       </div>
-    ) 
+    ); 
   }
 }
 
