a872e7abdcb333b7c7487f689afea5229823f794
ksm0207 <ksm03071@naver.com>
2020-12-10 06:11:53 +0900

    라우터 구축 학습

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {HashRouter,Route} from "react-router-dom"
+import Home from "./routes/Home"
+import About from "./routes/About"
 
 
 function App(){
   return(
-    <span>Public</span>
+    <HashRouter>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </HashRouter>
   )
 }
 
